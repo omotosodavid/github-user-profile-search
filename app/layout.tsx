d746cc8664c9e8cc8f5fe4fbe43e3f0130c21a48
omotosodavid <davidomotoso45+github@gmail.com>
2024-10-20 17:28:03 +0100

    fix: favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Github User Profile",
   description: "Web application that generates data based on searched username",
-  icons:"../public/logo.jpg",
+  icons:"logo.jpg",
   openGraph: { images: "logo.jpg", type: "website", locale: "en" },
 };
 
